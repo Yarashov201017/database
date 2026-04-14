5475b6ef5a3595bf8cc474846b9335a5e250bf29
Ulug'bek <ulugbeek256@gmail.com>
2026-04-14 15:39:15 +0500

    update

--- ddp.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-def cl_connect():
-    ish=psycopg2.connect(
-        host="localhost",
-        database="at_ishniklar",
-        user="postgres",
-        password="ulugbek"
-    )
-    return ish
